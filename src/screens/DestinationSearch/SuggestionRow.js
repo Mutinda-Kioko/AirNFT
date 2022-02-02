e59c265d1b91ab4ed655e4c831d34c6e61c9eeb2
Mutinda-Kioko <kiokojohn2275@gmail.com>
2022-02-02 16:41:57 +0300

    Added the Google Places autoComplete Feature

---
+++ src/screens/DestinationSearch/SuggestionRow.js
@@ -0,0 +1,15 @@
+import { View, Text,Pressable } from 'react-native';
+import React from 'react';
+import Entypo from '@expo/vector-icons/Entypo';
+import styles from "./styles.js";
+
+const SuggestionRow = ({item}) => (
+    <View style={styles.row}>
+        <View style={styles.iconContainer}>
+            <Entypo name='location-pin'  size={30}/>
+        </View>
+        <Text style={styles.locationText}>{item.description}</Text>
+    </View>
+)
+
+export default SuggestionRow;
--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,35 +1,36 @@
 import React, {useState} from 'react'
-import { View,Pressable, TextInput, FlatList, Text } from 'react-native'
+import { View, TextInput, FlatList, Text } from 'react-native'
 import styles from "./styles.js";
-import searchResult from '../../../assets/data/search';
-import Entypo from '@expo/vector-icons/Entypo';
 import { useNavigation } from "@react-navigation/native";
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete'
+import SuggestionRow from './SuggestionRow';
+
 
 const DestinationSearchScreen = () => {
     const navigation = useNavigation()
-    const [inputText, setInputText] = useState('')
     return (
-        <View style={styles.container}>
-            {/* Input*/}
-            <TextInput 
-             style={styles.textInput}
-             placeholder='Search for a house...'
-             value={inputText}
-             onChangeText={setInputText}
-            />
-            {/* list of components*/}
-            <FlatList
-            data={searchResult}
-            renderItem = {({item}) => (
-                <Pressable onPress={() => navigation.navigate('Guests')} style={styles.row}>
-                    <View style={styles.iconContainer}>
-                        <Entypo name='location-pin'  size={30}/>
-                    </View>
-                    <Text style={styles.locationText}>{item.description}</Text>
-                </Pressable>
-            )}
-            />
-        </View>
+            <View style={styles.container}>
+            <GooglePlacesAutocomplete
+            placeholder='Where are you going...'
+            onPress={(data, details = null) => {
+        // 'details' is provided when fetchDetails = true
+                console.log(data, details);
+                navigation.navigate('Guests'); 
+                }}
+        fetchDetails
+        styles={{
+             textInput:styles.textInput,
+
+        }}
+         query={{
+             key: 'AIzaSyBXDV_8PblqDGcnd0fRrj8FTW-WlUY2nbw',
+             language: 'en',
+             types: '(cities)',
+        }}
+        suppressDefaultStyles
+        renderRow={(item) => <SuggestionRow item={item}/>}
+    />
+            </View>
     )
 }
 
