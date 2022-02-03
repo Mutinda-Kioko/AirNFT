2f5cb3adf28c00bbe16fc20f7a74b6849f8adb8e
Mutinda-Kioko <kiokojohn2275@gmail.com>
2022-02-03 15:57:08 +0300

    Animated the map to the selected region.

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,5 +1,5 @@
 import { View, FlatList, useWindowDimensions } from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import MapView from 'react-native-maps';
 import { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import places from '../../../assets/data/feed';
@@ -10,10 +10,37 @@ import PostCarouselItem from '../../components/PostCarouselItem';
 
 const SearchResultsMap = () => {
 const [selectedPlaceId, setSelectedPlaceId] = useState(null);
-const width = useWindowDimensions().width
+const width = useWindowDimensions().width;
+const flatlist = useRef();
+const maP = useRef();
+const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+const onViewChanged = useRef(({viewableItems}) => {
+  if (viewableItems.length > 0) {
+    const selectedPlace = viewableItems[0].item
+    setSelectedPlaceId(selectedPlace.id)
+  }
+})
+
+useEffect(() => {
+if (!selectedPlaceId || !flatlist) {
+  return;
+}
+const index = places.findIndex(place => place.id === selectedPlaceId)
+flatlist.current.scrollToIndex({index});
+const selectedPlace = places[index];
+const region = {
+  latitude: selectedPlace.coordinate.latitude,
+  longitude: selectedPlace.coordinate.longitude,
+  latitudeDelta: 0.8,
+  longitudeDelta: 0.8,
+}
+maP.current.animateToRegion(region);
+}, [selectedPlaceId]);
+
 return (
     <View style={{width:'100%', height:'100%'}}>
     <MapView
+    ref={maP}
     style={{width:'100%', height:'100%'}}
     provider={PROVIDER_GOOGLE}
     initialRegion={{
@@ -34,6 +61,7 @@ return (
   </MapView>
   <View style={{position:'absolute', bottom: 50}}>
     <FlatList
+    ref={flatlist}
     data={places}
     horizontal
     renderItem={({item}) => <PostCarouselItem post={item}/> }
@@ -41,6 +69,8 @@ return (
     snapToInterval={width - 60}
     snapToAlignment={'center'}
     decelerationRate={'fast'}
+    viewabilityConfig={viewConfig.current}
+    onViewableItemsChanged={onViewChanged.current}
     />
   </View>
     </View>
